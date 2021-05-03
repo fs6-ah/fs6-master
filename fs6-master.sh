7d7e9ae8a6836a2e66380063ce09bba2a36b1ae3
/FS6 <fs6ahso@gmail.com>
2021-05-03 07:56:13 +0000

    Add files via upload

---
+++ fs6-master.sh
@@ -0,0 +1,15 @@
+printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+chmod +x ngrok
+printf "\e[1;92m[\e[0m*\e[1;92m] Done Download Ngrok...\n"
+rm -rf ngrok-stable-linux-arm.zip
+else
+printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+exit 1
+fi
+
+
